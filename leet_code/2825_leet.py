4335d7f79ab30952abf2017cb0ebe1a2ec15f743
unknown <snehasishsen03@gmail.com>
2024-12-04 09:38:20 +0530

    2825 potd

---
+++ leet_code/2825_leet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        
+        pass
+
+
+str1 = "abc"
+str2 = "ad"
+
+sol = Solution()
+ans = sol.canMakeSubsequence(str1,str2)
+print(ans)
